8acae741cf1ef2fec986db263cd975d157f61cab
Manuel Cabezas <manuelcabezas@gmail.com>
2022-12-11 00:24:46 -0400

    feat: add anonymous function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-        // function pintar() {
-        //     ele = document.getElementById("ele1")
-        //     ele.style.backgroundColor = 'yellow'
-        // }
-        const pintar = (color) => ele.style.backgroundColor = color
+/*
+        function pintar(){
+        ele = document.getElementById("ele1")
+        ele.style.backgroundColor = 'yellow'
+        }
+        ele = document.getElementById("ele1")
+        ele.addEventListener("click", pintar);
+*/
+
+        const pintar = (color = "green") => ele.style.backgroundColor = color
         let ele = document.getElementById("ele1");
-        ele.style.backgroundColor = "green";
         ele.addEventListener("click",() => pintar("yellow"));
